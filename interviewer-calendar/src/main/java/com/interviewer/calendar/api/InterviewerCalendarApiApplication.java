ab4604c81c8a6cb042771dfbfa7cff6abbbfe78c
DANIELA SPITZER <dsdanielaspitzer@gmail.com>
2023-05-09 09:56:52 +0100

    packages

---
+++ interviewer-calendar/src/main/java/com/interviewer/calendar/api/InterviewerCalendarApiApplication.java
@@ -0,0 +1,13 @@
+package com.interviewer.calendar.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewerCalendarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewerCalendarApiApplication.class, args);
+	}
+
+}
---
+++ interviewer-calendar/src/test/java/com/interviewer/calendar/api/InterviewerCalendarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interviewer.calendar.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewerCalendarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
